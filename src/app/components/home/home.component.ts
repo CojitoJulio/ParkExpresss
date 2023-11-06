783a59cca6ae851def94b095092867769fd59895
CojitoJulio <dani.gutierrezg@duocuc.cl>
2023-11-06 08:58:26 -0300

    Se agregaron los puntos de parking

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MapService } from '@core/services/map.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,51 @@ import { MapService } from '@core/services/map.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private map: MapService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.map.buildMap();
+    const estacionamientos = [
+      {
+        lon: -33.433157,
+        lat: -70.6157,
+        name: 'Santa Isabel',
+      },
+      {
+        lon: -33.431923,
+        lat: -70.616467,
+        name: 'General del Canto',
+      },
+      {
+        lon: -33.43314,
+        lat: -70.613058,
+        name: 'Fernández Concha',
+      },
+      {
+        lon: -33.434816,
+        lat: -70.616431,
+        name: 'Luis Carrera',
+      },
+    ];
+
+    const CustomIcon = L.icon({
+      iconUrl: '../../../assets/parking-icon.png',
+      iconSize: [40, 40],
+    });
+
+    const map = L.map('map').setView([-33.433157, -70.6157], 17);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    estacionamientos.forEach((parking) => {
+      L.marker([parking.lon, parking.lat], {
+        title: parking.name,
+        icon: CustomIcon,
+      })
+        .bindPopup(parking.name)
+        .addTo(map);
+    });
   }
 }
