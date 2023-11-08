8b8a21a1f0ddb1289a20bc57b41738c215a84fde
CojitoJulio <dani.gutierrezg@duocuc.cl>
2023-11-08 17:00:26 -0300

    Se agregar disenios pulentos
    el disenio del auto y el de 404

--- src/app/components/addcar/addcar.component.ts
+++ src/app/components/addcar/addcar.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-addcar',
   templateUrl: './addcar.component.html',
-  styleUrls: ['./addcar.component.css']
+  styleUrls: ['./addcar.component.css'],
 })
 export class AddcarComponent {
+  patente: string = '';
 
+  soloNumeros(event: any) {
+    const tecla = event.key;
+    if (
+      !/^[0-9]$/.test(tecla) &&
+      tecla !== 'Backspace' &&
+      tecla !== 'Delete' &&
+      tecla !== 'ArrowLeft' &&
+      tecla !== 'ArrowRight' &&
+      tecla !== 'Tab'
+    ) {
+      event.preventDefault();
+    }
+  }
 }
